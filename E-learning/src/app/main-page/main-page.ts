f5b32e2ff47319438d3a2b09b49a110ec12891d4
LukaTsak <lukawaqadze01@gmail.com>
2026-04-06 01:55:00 +0400

    added headder, both authorised and unauthorised versions

--- E-learning/src/app/main-page/main-page.ts
+++ E-learning/src/app/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header'
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Header],
+  imports: [Header,Footer],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
