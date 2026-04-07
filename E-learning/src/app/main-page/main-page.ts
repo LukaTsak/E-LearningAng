7c5915c640dca0ca8c3b7b1b46ae3fb91ec04c89
LukaTsak <lukawaqadze01@gmail.com>
2026-04-08 00:05:11 +0400

    added login modal

--- E-learning/src/app/main-page/main-page.ts
+++ E-learning/src/app/main-page/main-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header'
 import { Footer } from '../footer/footer';
+import { Login } from '../authorisationElements/login/login';
+import { SignUp } from '../authorisationElements/sign-up/sign-up';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Header,Footer],
+  imports: [Header,Footer,Login,SignUp],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
